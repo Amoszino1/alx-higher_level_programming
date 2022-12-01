6480af079aac676db4231e611ab00caf8d3f5ba3
Amoszino1 <oyelekeamos744@gmail.com>
2022-12-01 15:01:49 -0800

    1-calculation.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
+import string
+print(string.ascii_uppercase)
 
-if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
-    from add_0 import add
-
-a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
