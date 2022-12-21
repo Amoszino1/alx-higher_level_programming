9657390fe796db1189ee7655bc702a8793c19ea2
Amoszino1 <oyelekeamos744@gmail.com>
2022-12-21 05:39:51 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-# 0-square.py by Daniel Successful
-"""Defines a square """
+"""Define a class Square."""
 
 
 class Square:
-    """Does nothing"""
-    pass
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
