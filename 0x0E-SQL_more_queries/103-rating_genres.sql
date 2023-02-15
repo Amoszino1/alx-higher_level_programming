fd0c1f51aee3f3d32c74719c59c8d18b708dc68c
Amoszino1 <oyelekeamos744@gmail.com>
2023-02-15 12:25:11 -0800

    103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,7 +3,7 @@
 SELECT `name`, SUM(`rate`) AS `rating`
   FROM `tv_genres` AS g
        INNER JOIN `tv_show_genres` AS s
-       ON s.`genre_id` = g.`id
+       ON s.`genre_id` = g.`id`
 
        INNER JOIN `tv_show_ratings` AS r
        ON r.`show_id` = s.`show_id`
